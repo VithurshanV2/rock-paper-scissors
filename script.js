9f8845a75e38ed9d67935f20a849da9b8161637d
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-01-27 12:03:04 +0530

    Initial commit with project setup
    - add index.html
    - add script.js
    - implement getComputerChoice function

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    return choices[randomNumber];
+}
+
+console.log(getComputerChoice());
